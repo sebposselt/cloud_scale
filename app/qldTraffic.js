edc6294cba309e4e52f56a79ab45f9eacf2b5471
juliehaga <jul.haga@gmail.com>
2018-10-19 06:13:25 +0200

    fetching all webcams from API

--- app/qldTraffic.js
+++ app/qldTraffic.js
@@ -1,19 +1,19 @@
 var unirest = require('unirest');
-apikey = "3e83add325cbb69ac4d8e5bf433d770b";
+let apikey = "3e83add325cbb69ac4d8e5bf433d770b";
+
+
 
 let APIinfo = {
     "hostname" : "https://api.qldtraffic.qld.gov.au/",
     "path" : "v1/webcams/",
-    "webCamID": "1",
     "APIkey": "?apikey=" + apikey
 };
 
 
 
-exports.QLDtrafficAPIRequest = function(webCamID)  {
-    APIinfo.webCamID = webCamID;
+exports.QLDtrafficAPIRequest = function()  {
     return new Promise(function(resolve, reject) {
-        unirest.get(APIinfo.hostname + APIinfo.path + APIinfo.webCamID + APIinfo.APIkey)
+        unirest.get(APIinfo.hostname + APIinfo.path + APIinfo.APIkey)
             .header("X-Mashape-Key", "JCOm6HzntkmshkTLOE6Omng73CKap1Xh0cdjsnhhOk5IdC253g")
             .header("Accept", "text/plain")
             .end(function (result) {
@@ -28,14 +28,25 @@ exports.QLDtrafficAPIRequest = function(webCamID)  {
     })
 };
 
-exports.extractImages = function(response){
-    //store the data???
-    
-    for (let i = 0; i < response.length; i++) {
-        console.log(response[i].features[0].properties.id);
-        console.log(response[i].features[0].properties.image_url);
-        console.log("****************************************************");
+
+exports.filterTrafficData = function (webcamList, allTrafficData){
+    let webCamData = [];
+    for (let i = 0; i < allTrafficData.features.length; i++) {
+        if (webcamList.includes(String(allTrafficData.features[i].properties.id))){
+            webCamData.push(allTrafficData.features[i]);
+        }
     }
+    return webCamData;
+};
+
+
+
+exports.extractImageURL = function(webcamData){
+    let webcamImage = {
+        "id" : webcamData[i].properties.id,
+        "url" : webcamData[i].properties.image_url
+    };
+    return webcamImage;
 };
 
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,35 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var datamanager = require('../datamanager');
+var qldTraffic = require('../qldTraffic');
 
 
 var webCamlist = ["1", "3", "117"];
-var promises = [];
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    qldTraffic.QLDtrafficAPIRequest().then(function whenOk(allTrafficData) {
+        let webcamData = qldTraffic.filterTrafficData(webCamlist,allTrafficData);
+        qldTraffic.extractImageURL(webcamData);
+        res.render('index', { title: 'Express' });
+    }).catch((error) => {
+        console.log(error);
 
-
-  //should probably be a post route. Where the webcam ids comes from the user.
-
-  for (let i = 0; i < webCamlist.length; i++) {
-    console.log(webCamlist[i]);
-    promises.push(datamanager.QLDtrafficAPIRequest(webCamlist[i]));
-  }
-
-
-  Promise.all(promises).then(function whenOk(response) {
-      datamanager.extractImages(response);
-
-      res.render('index', { title: 'Express' });
-  }).catch((error) => {
-      console.log("promise failed");
   });
-
-
 });
 
 module.exports = router;
