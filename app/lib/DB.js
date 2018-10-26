117469bc5344d456d7ed6d167a1d5486f5ffeb6e
Sebastian P <npm981@ku.dk>
2018-10-26 16:06:24 +1000

    DB lib working

---
+++ app/lib/DB.js
@@ -0,0 +1,160 @@
+const mongoClient = require("mongodb").MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = "mongodb://cab432:b4a7SvudCNdHlNg04looE6KsbwxanHSIGnLmXpsXk34UMVpzWKFWyFPWaTSABo0cilJZ0E4p45h7ifr3F4XjIQ%3D%3D@cab432.documents.azure.com:10255/?ssl=true"
+// Database and Collection Names
+const dbName = 'MaybeCars';
+const collectionName = 'MaybeCars';
+
+
+const insertPicture = function(arrOfObjs,db,callback) {
+    // Get the documents collection
+    const collection = db.collection(collectionName);
+    // Insert stuff
+    collection.insertMany(arrOfObjs, function (err, result) {
+        len = arrOfObjs.length;
+        assert.equal(err, null);
+        assert.equal(len, result.result.n);
+        assert.equal(len, result.ops.length);
+        //error handling stuff done here
+        console.log("Inserted " + len+ " pictures to DB");
+        callback(result);
+    });
+}
+
+const removeDocument = function (IDofObj, db, callback) {
+    // Get the documents collection
+    const collection = db.collection(collectionName);
+    // Delete document where a is 3
+    collection.deleteOne({ "cam": IDofObj }, function (err, result) {
+        assert.equal(err, null);
+        assert.equal(1, result.result.n);
+        console.log("Removed the document with id: ", IDofObj);
+        callback(result);
+    });
+}
+
+const updateDocument = function (id, img, db, callback) {
+    // Get the documents collection
+    const collection = db.collection(collectionName);
+
+    collection.updateOne(
+        { "cam": id },
+        { $set: { "pic": img } }, function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document with id: ", id);
+            callback(result);
+        });
+}
+
+const findDocuments = function (id, db, callback) {
+    // Get the documents collection
+    const collection = db.collection(collectionName);
+    // Find some documents
+    collection.find({ 'cam': id }).toArray(function (err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+        // return stuff
+        callback(docs);
+    });
+}
+
+exports.findImage = function (IDofObj) {
+    mongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        // the target DB
+        const db = client.db(dbName);
+        findDocuments(IDofObj, db, function () {
+            client.close();
+        });
+    });
+}
+
+
+exports.updateImage = function (IDofObj,newPic) {
+    mongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        // the target DB
+        const db = client.db(dbName);
+        updateDocument(IDofObj,newPic, db, function () {
+            client.close();
+        });
+    });
+}
+
+exports.deleteOne =function(IDofObj){
+    mongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        // the target DB
+        const db = client.db(dbName);
+
+        removeDocument(IDofObj, db, function () {
+            client.close();
+        });
+    });
+}
+
+exports.pushPicture = function (arrOfObjs) {
+    mongoClient.connect(url, function (err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        // the target DB
+        const db = client.db(dbName);
+
+        insertPicture(arrOfObjs,db,function () {
+            client.close();
+        });
+    });
+}
+
+
+
+
+// const updateDocument = function (id,img, db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('documents');
+//     // Update document where a is 2, set b equal to 1
+//     collection.updateOne({ "id": id }
+//         , { $set: { pic: img } }, function (err, result) {
+//             assert.equal(err, null);
+//             assert.equal(1, result.result.n);
+//             console.log("Updated the document with the field a equal to 2");
+//             callback(result);
+//         });
+// }
+
+// const findDocuments = function (db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection(collectionName);
+//     // Find some documents
+//     collection.find({ 'a': 3 }).toArray(function (err, docs) {
+//         assert.equal(err, null);
+//         console.log("Found the following records");
+//         console.log(docs);
+//         callback(docs);
+//     });
+// }
+
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// // Database Name
+// const dbName = 'myproject';
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function (err, client) {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
+
+//     const db = client.db(dbName);
+
+//     insertDocuments(db, function () {
+//         client.close();
+//     });
+// });
