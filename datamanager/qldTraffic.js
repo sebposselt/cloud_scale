ac9b356ea87631ed181e05cca686e0df6297b07c
Sebastian P <npm981@ku.dk>
2018-10-30 17:02:57 +1000

    cleanup

--- datamanager/qldTraffic.js
+++ datamanager/qldTraffic.js
@@ -1,4 +1,3 @@
-var unirest = require('unirest');
 let apikey = "3e83add325cbb69ac4d8e5bf433d770b";
 
 
@@ -6,11 +5,11 @@ let apikey = "3e83add325cbb69ac4d8e5bf433d770b";
 let webcamList = [];
 
 
-let APIinfo = {
-    "hostname" : "https://api.qldtraffic.qld.gov.au/",
-    "path" : "v1/webcams/",
-    "APIkey": "?apikey=" + apikey
-};
+// let APIinfo = {
+//     "hostname" : "https://api.qldtraffic.qld.gov.au/",
+//     "path" : "v1/webcams/",
+//     "APIkey": "?apikey=" + apikey
+// };
 
 
 exports.addCameras = function(reqBody){
@@ -30,58 +29,58 @@ exports.removeCamera = function(webcamID){
 };
 
 
-exports.QLDtrafficAPIRequest = function()  {
-    return new Promise(function(resolve, reject) {
-        unirest.get(APIinfo.hostname + APIinfo.path + APIinfo.APIkey)
-            .header("X-Mashape-Key", "JCOm6HzntkmshkTLOE6Omng73CKap1Xh0cdjsnhhOk5IdC253g")
-            .header("Accept", "text/plain")
-            .end(function (result) {
-                if (result.error) {
-                    console.log("Call to API failed")
-                    reject(result.error);
-                } else {
-                    console.log("Call to API succeded");
-                    resolve(result.body);
-                }
-            });
-    })
-};
+// exports.QLDtrafficAPIRequest = function()  {
+//     return new Promise(function(resolve, reject) {
+//         unirest.get(APIinfo.hostname + APIinfo.path + APIinfo.APIkey)
+//             .header("X-Mashape-Key", "JCOm6HzntkmshkTLOE6Omng73CKap1Xh0cdjsnhhOk5IdC253g")
+//             .header("Accept", "text/plain")
+//             .end(function (result) {
+//                 if (result.error) {
+//                     console.log("Call to API failed")
+//                     reject(result.error);
+//                 } else {
+//                     console.log("Call to API succeded");
+//                     resolve(result.body);
+//                 }
+//             });
+//     })
+// };
 
 
 //filters out the cameras the user has requested
 //må kunne slå op på client
 
-exports.filterTrafficData = function (allTrafficData, sessID){
-    let webcamData = [];
-    for (let i = 0; i < allTrafficData.features.length; i++) {
-        if (webcamList.includes(String(allTrafficData.features[i].properties.id))){
-            console.log("Webcam    " + allTrafficData.features[i].properties.id);
-            webcamData.push(allTrafficData.features[i]);
-        }
-    }
-    return webcamData;
-};
+// exports.filterTrafficData = function (allTrafficData, sessID){
+//     let webcamData = [];
+//     for (let i = 0; i < allTrafficData.features.length; i++) {
+//         if (webcamList.includes(String(allTrafficData.features[i].properties.id))){
+//             console.log("Webcam    " + allTrafficData.features[i].properties.id);
+//             webcamData.push(allTrafficData.features[i]);
+//         }
+//     }
+//     return webcamData;
+// };
 
 
 
 
-exports.extractImageURLAndID = function(webcamData){
-    let webcamImage = {
-        "id" : webcamData.properties.id,
-        "url" : webcamData.properties.image_url
-    };
-    return webcamImage;
-};
+// exports.extractImageURLAndID = function(webcamData){
+//     let webcamImage = {
+//         "id" : webcamData.properties.id,
+//         "url" : webcamData.properties.image_url
+//     };
+//     return webcamImage;
+// };
 
 
-exports.cleanList = function(allTrafficData){
-    let res = [];
-    for (let i = 0; i < allTrafficData.features.length; i++) {
-        const elm = allTrafficData.features[i];
-        let tmp = {"id": elm.properties.id, "description":elm.properties.description};
-        //console.log(tmp, elm.properties.description)
-        res.push(tmp);
-        // res.push(elm.properties.description);
-    }
-    return res;
-};
+// exports.cleanList = function(allTrafficData){
+//     let res = [];
+//     for (let i = 0; i < allTrafficData.features.length; i++) {
+//         const elm = allTrafficData.features[i];
+//         let tmp = {"id": elm.properties.id, "description":elm.properties.description};
+//         //console.log(tmp, elm.properties.description)
+//         res.push(tmp);
+//         // res.push(elm.properties.description);
+//     }
+//     return res;
+// };
