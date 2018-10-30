d6044600501677ae350a791522152c41122540b3
Sebastian P <npm981@ku.dk>
2018-10-30 16:54:28 +1000

    new dir

---
+++ datamanager/lib/ObjDetection.js
@@ -0,0 +1,91 @@
+const request = require('request-promise');
+const { drawRect, } = require('./utils');
+const utils = require('./utils');
+const cv = require("opencv4nodejs");
+const fs = require('fs');
+const path = require('path');
+const classNames = require('./dnnCocoClassNames');
+const { extractResults } = require('./dnn/ssdUtils');
+
+
+
+// code copied from https://github.com/justadudewhohacks/opencv4nodejs/tree/master/examples under MIT-licence
+// **********************************************************************************************************
+if (!cv.xmodules.dnn) {
+  throw new Error('exiting: opencv4nodejs compiled without dnn module');
+}
+
+// replace with path where you unzipped inception model
+const ssdcocoModelPath = path.join(__dirname, 'model', 'VGGNet', 'coco', 'SSD_300x300');
+
+const prototxt = path.resolve(ssdcocoModelPath, 'deploy.prototxt');
+const modelFile = path.resolve(ssdcocoModelPath, 'VGG_coco_SSD_300x300_iter_400000.caffemodel');
+
+if (!fs.existsSync(prototxt) || !fs.existsSync(modelFile)) {
+  console.log('could not find ssdcoco model');
+  console.log('download the model from: https://drive.google.com/file/d/0BzKzrI_SkD1_dUY1Ml9GRTFpUWc/view');
+  throw new Error('exiting: could not find ssdcoco model');
+}
+
+// initialize ssdcoco model from prototxt and modelFile
+const net = cv.readNetFromCaffe(prototxt, modelFile);
+
+function classifyImg(img) {
+  // ssdcoco model works with 300 x 300 images
+  const imgResized = img.resize(300, 300);
+
+  // network accepts blobs as input
+  const inputBlob = cv.blobFromImage(imgResized);
+  net.setInput(inputBlob);
+
+  // forward pass input through entire network, will return
+  // classification result as 1x1xNxM Mat
+  let outputBlob = net.forward();
+  // extract NxM Mat
+  outputBlob = outputBlob.flattenFloat(outputBlob.sizes[2], outputBlob.sizes[3]);
+  //console.log(extractResults(outputBlob, img));
+  return extractResults(outputBlob, img)
+    .map(r => Object.assign({}, r, { className: classNames[r.classLabel] }));
+}
+
+const makeDrawClassDetections = predictions => (drawImg, className, getColor, thickness = 2) => {
+  predictions
+    .filter(p => classNames[p.classLabel] === className)
+    .forEach(p => drawRect(drawImg, p.rect, getColor(), { thickness }));
+  return drawImg;
+};
+// **********************************************************************************************************
+
+
+
+// Summery: runs object detection on a picture, saves the resulting image with bounding boxes around found objs
+// Input: obj of type {id:,url:} and minConf is optional.
+// Output:
+// Error:
+// Comment:
+exports.runDetect = async function (obj, minConf = 0.3) {
+  let id = obj.id;
+  let url = obj.url;
+  try {
+    await utils.saveImgFromUrl(id, url);
+    console.log('runDetect: image should be saved');
+    let filenameR = "cam" + id + ".jpg";
+    let fullFilenameR = path.join(__dirname, '..', 'data', filenameR)
+    let filenameW = "DONEcam" + id + ".jpg";
+    let fullFilenameW = path.join(__dirname, '..', 'data', filenameW)
+    const img = await cv.imreadAsync(fullFilenameR);
+
+    const minConfidence = minConf
+    const predictions = classifyImg(img).filter(res => res.confidence > minConfidence);
+    const drawClassDetections = makeDrawClassDetections(predictions);
+    const getRandomColor = () => new cv.Vec(Math.random() * 255, Math.random() * 255, 255);
+    drawClassDetections(img, 'car', getRandomColor);
+
+    cv.imwrite(fullFilenameW, img);
+    return { "id": id, img: utils.convertImgToBASE64(img) };
+
+  } catch (error) {
+    console.log("runDetect IO error: ", error);
+  }
+}
+
---
+++ datamanager/lib/utils.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const fs = require('fs');
+const request = require('request-promise');
+const cv = require("opencv4nodejs");
+
+// code copied from https://github.com/justadudewhohacks/opencv4nodejs/tree/master/examples under MIT-licence
+// **********************************************************************************************************
+const dataPath = path.resolve(__dirname, '../data');
+exports.dataPath = dataPath;
+exports.getDataFilePath = fileName => path.resolve(dataPath, fileName);
+
+exports.grabFrames = (videoFile, delay, onFrame) => {
+  const cap = new cv.VideoCapture(videoFile);
+  let done = false;
+  const intvl = setInterval(() => {
+    let frame = cap.read();
+    // loop back to start on end of stream reached
+    if (frame.empty) {
+      cap.reset();
+      frame = cap.read();
+    }
+    onFrame(frame);
+    const key = cv.waitKey(delay);
+    done = key !== -1 && key !== 255;
+    if (done) {
+      clearInterval(intvl);
+      console.log('Key pressed, exiting.');
+    }
+  }, 0);
+};
+exports.drawRectAroundBlobs = (binaryImg, dstImg, minPxSize, fixedRectWidth) => {
+  const {
+    centroids,
+    stats
+  } = binaryImg.connectedComponentsWithStats();
+
+  // pretend label 0 is background
+  for (let label = 1; label < centroids.rows; label += 1) {
+    const [x1, y1] = [stats.at(label, cv.CC_STAT_LEFT), stats.at(label, cv.CC_STAT_TOP)];
+    const [x2, y2] = [
+      x1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_WIDTH)),
+      y1 + (fixedRectWidth || stats.at(label, cv.CC_STAT_HEIGHT))
+    ];
+    const size = stats.at(label, cv.CC_STAT_AREA);
+    const blue = new cv.Vec(255, 0, 0);
+    if (minPxSize < size) {
+      dstImg.drawRectangle(
+        new cv.Point(x1, y1),
+        new cv.Point(x2, y2),
+        { color: blue, thickness: 2 }
+      );
+    }
+  }
+};
+const drawRect = (image, rect, color, opts = { thickness: 2 }) =>
+  image.drawRectangle(
+    rect,
+    color,
+    opts.thickness,
+    cv.LINE_8
+  );
+exports.drawRect = drawRect;
+exports.drawBlueRect = (image, rect, opts = { thickness: 2 }) =>
+  drawRect(image, rect, new cv.Vec(255, 0, 0), opts);
+exports.drawGreenRect = (image, rect, opts = { thickness: 2 }) =>
+  drawRect(image, rect, new cv.Vec(0, 255, 0), opts);
+exports.drawRedRect = (image, rect, opts = { thickness: 2 }) =>
+  drawRect(image, rect, new cv.Vec(0, 0, 255), opts);
+
+// **********************************************************************************************************
+
+
+exports.cv = cv;
+// Summery: Saves an image from an URL in the DATA dir. namingscheme: cam<ID>.jpg
+// Input: 
+// Output: a promise to be handled by the caller
+// Error:
+// Comment: Is used async by the caller.
+exports.saveImgFromUrl = (id, url) => {
+  return new Promise((resolve, reject) => {
+    let filename = "cam" + id + ".jpg";
+    let fullFilename = path.join(__dirname, '..', 'data', filename)
+    request(url, { encoding: 'binary' }, function (error, response, body) {
+      if (error) reject(error)
+
+      fs.writeFile(fullFilename, body, 'binary', function (err) {
+        if (err) {
+          console.log("Error saving imagefile: ", filename);
+          console.log("Error:", err);
+          reject(err)
+        }
+        resolve()
+      });
+    })
+  })
+};
+
+exports.convertImgToBASE64 = (img) => {
+  return cv.imencode(".jpg", img).toString('base64');
+};
+exports.convertBASE64ToImg = (base64imgstring) => {
+  const base64text = base64imgstring;//Base64 encoded string
+  const base64data = base64text.replace('data:image/jpg;base64', '')
+    .replace('data:image/png;base64', '');//Strip image type prefix
+  const buffer = Buffer.from(base64data, 'base64');
+  const image = cv.imdecode(buffer); //Image is now represented as Mat
+  return image;
+}
